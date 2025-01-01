8a2c4493bca4534b3e88e99a46feaeb533ce4919
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2025-01-01 20:10:27 +0300

    Update 2.py

--- Solution/Day 1/2.py
+++ Solution/Day 1/2.py
@@ -1,3 +1,10 @@
 # approach
-class SOlution:
-    pass
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        mn = float('inf')
+        res = 0
+
+        for price in prices:
+            mn = min(mn , price)
+            res = max(res  , price - mn)
+        return res
