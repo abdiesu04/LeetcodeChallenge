676a125f353c007dc544142b623f0bef6272b3b8
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2025-01-02 22:17:10 +0300

    Update 1.py

--- Solution/Day 2/1.py
+++ Solution/Day 2/1.py
@@ -1,10 +1,41 @@
 # approach
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        mn = float('inf')
-        res = 0
+"""
+Use a bfs and collect the videos at a level and sort in the dctionary and return 
 
-        for price in prices:
-            mn = min(mn , price)
-            res = max(res  , price - mn)
-        return res
+"""
+class Solution:
+    def watchedVideosByFriends(self, watchedVideos: List[List[str]], friends: List[List[int]], id: int, level: int) -> List[str]:
+        graph = defaultdict(list)
+        for u, v in enumerate(friends):  
+            for f in v:
+                graph[u].append(f)
+        
+        queue = deque([id])
+        visited = {id}
+        stage = 0
+
+        while queue:
+            if stage == level:
+                curr = []
+                for _ in range(len(queue)):
+                    node = queue.popleft()
+                    curr.append(node)
+                break
+
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                for child in graph[node]:
+                    if child not in visited:
+                        visited.add(child)
+                        queue.append(child)
+            stage += 1
+        else:
+            curr = []
+        print(curr)
+        video_count = defaultdict(int)
+        for friend in curr:
+            for video in watchedVideos[friend]:
+                video_count[video] += 1
+
+        result = sorted(video_count.keys(), key=lambda x: (video_count[x], x))
+        return result
